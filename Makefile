0856a135ada78191081e15dac6b3febf88568410
Maxim Krentovskiy <mkrentovskiy@gmail.com>
2014-10-18 00:06:01 +0400

    Add some ALSA support but its not work yet.

--- Makefile
+++ Makefile
@@ -1,3 +1,20 @@
+TOOLCHAINS_DIR = /home/mkrentovskiy/develop/virt2real/install-sdk/codesourcery/arm-2013.05
+SYSROOT = /home/mkrentovskiy/develop/virt2real/virt2real-sdk.lxc/fs/output/host/usr/arm-buildroot-linux-gnueabi/sysroot
+
+OPTS = GOARCH=arm
+OPTS += GOARM=5 
+OPTS += GOOS=linux
+OPTS += CGO_ENABLED=1 
+OPTS += CC="arm-none-linux-gnueabi-gcc"
+OPTS += CXX=arm-none-linux-gnueabi-g++
+OPTS += PATH=$(PATH):$(TOOLCHAINS_DIR)/bin 
+OPTS += PKG_CONFIG_DIR=""
+OPTS += PKG_CONFIG_LIBDIR=$(SYSROOT)/usr/lib/pkgconfig
+OPTS += PKG_CONFIG_SYSROOT_DIR=$(SYSROOT)
+OPTS += CGO_CFLAGS="-I$(SYSROOT)/usr/include"
+OPTS += CGO_LDFLAGS="-L$(SYSROOT)/usr/lib -Wl,-rpath,$(SYSROOT)/usr/lib --sysroot=$(SYSROOT)"
+
 all:
-	GOARCH=arm GOARM=5 CGO_ENABLED=1 go build
-	scp ambient root@192.168.222.125:/root
+	$(OPTS) go env
+	$(OPTS) go build
+	scp ambient root@192.168.3.1:/root
--- ambient.go
+++ ambient.go
@@ -1,34 +1,125 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
 	"github.com/mkrentovskiy/ambient/devices"
+	"github.com/tryphon/alsa-go"
 	"time"
 )
 
-func main() {
-	num_leds := uint(160)
-	leds := make([]devices.RGB, num_leds)
+func sound_in(dest chan int) {
+	handle := alsa.New()
+	err := handle.Open("default", alsa.StreamTypeCapture, alsa.ModeBlock)
+	if err != nil {
+		fmt.Printf("Open failed. %s", err)
+		return
+	}
 
-	state := devices.InitWS2801(0, 1, num_leds)
-	for j := byte(0); j < 255; j++ {
-		for i, _ := range leds {
-			if i%6 == 0 {
-				leds[i] = devices.RGB{j % 32, 0, 0}
-			} else if i%6 == 1 {
-				leds[i] = devices.RGB{0, j % 32, 0}
-			} else if i%6 == 2 {
-				leds[i] = devices.RGB{0, 0, j % 32}
-			} else if i%6 == 3 {
-				leds[i] = devices.RGB{j % 32, j % 64, 0}
-			} else if i%6 == 4 {
-				leds[i] = devices.RGB{j % 32, 0, j % 64}
-			} else if i%6 == 6 {
-				leds[i] = devices.RGB{0, j % 32, j % 64}
-			}
+	handle.SampleFormat = alsa.SampleFormatS16LE
+	handle.SampleRate = 4000
+	handle.Channels = 1
+	err = handle.ApplyHwParams()
+	if err != nil {
+		fmt.Printf("SetHwParams failed. %s", err)
+		return
+	}
 
+	buflen := int(16 * 1024)
+	buf := make([]uint8, buflen)
+	for {
+		n, err := handle.Read(buf)
+		if err != nil {
+			fmt.Printf("Read failed. %s", err)
+		} else if n == buflen {
+			go func(b []uint8) {
+				var v int16
+				s := float32(0)
+				br := bytes.NewReader(b)
+				for binary.Read(br, binary.LittleEndian, &v) == nil {
+					if s > 0 {
+						s = (s + float32(v)) / 2
+					} else {
+						s = float32(v)
+					}
+				}
+				if s > 0 {
+					dest <- int(s)
+				}
+			}(buf)
 		}
-		time.Sleep(120 * time.Millisecond)
+	}
+	handle.Close()
+}
+
+func pulse(src chan int, dest chan int, tres int, pulse_ms int) {
+	k := 0
+	for {
+		go func() {
+			v := <-src
+			// fmt.Printf(" -> %d\n", v)
+			if v > tres {
+				// fmt.Printf(" -> %d\n", v)
+				k = v
+			}
+		}()
+		if k > 0 {
+			dest <- k
+			k = 0
+		} else {
+			dest <- 0
+		}
+		time.Sleep(time.Duration(pulse_ms) * time.Millisecond)
+	}
+}
+
+func leds(source chan int, num_leds uint, min_val int, max_val int) {
+	leds := make([]devices.RGB, num_leds)
+	state := devices.InitWS2801(0, 1, num_leds)
+
+	for {
+		go func() {
+			val := <-source
+			for i := num_leds - 1; i > 0; i-- {
+				leds[i] = leds[i-1]
+			}
+			leds[0] = val_to_color(val, min_val, max_val)
+		}()
 		state.Send(leds)
+		time.Sleep(100 * time.Millisecond)
 	}
 	state.Done()
+}
+
+func val_to_color(val int, min_val int, max_val int) devices.RGB {
+	if val < min_val || val > max_val {
+		return devices.RGB{0, 0, 0}
+	}
+
+	var h float32 = float32(val-min_val) * 360 / float32(max_val-min_val)
+	// fmt.Printf(" _ h = %f \n", h)
+
+	if h >= 0 && h < 60 {
+		return devices.RGB{byte(255), byte(255 * (1 - h/59)), byte(0)}
+	} else if h >= 60 && h < 120 {
+		return devices.RGB{byte(255 * (1 - h/59)), byte(255), byte(0)}
+	} else if h >= 120 && h < 180 {
+		return devices.RGB{byte(0), byte(255), byte(255 * (1 - h/59))}
+	} else if h >= 180 && h < 240 {
+		return devices.RGB{byte(0), byte(255 * (1 - h/59)), byte(255)}
+	} else if h >= 240 && h < 300 {
+		return devices.RGB{byte(255 * (1 - h/59)), byte(0), byte(255)}
+	} else if h >= 300 && h <= 360 {
+		return devices.RGB{byte(255), byte(0), byte(255 * (1 - h/59))}
+	}
+	return devices.RGB{0, 0, 0}
+}
+
+func main() {
+	chp := make(chan int)
+	chl := make(chan int)
+	go pulse(chp, chl, 300, 10)
+	go leds(chl, 160, 300, 2000)
+	sound_in(chp)
 }
